a375054e55a5941f054dfb34467d3e333b5e4c9a
nicbongo <nicbongo@protonmail.com>
2020-01-21 16:27:34 -0400

    start

---
+++ todo-w-reducer/src/components/ToDoItem.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ToDoItem = () => {
+  return (
+    <div>
+      <p>ToDoItem</p>
+    </div>
+  )
+}
+
+export default ToDoItem;
---
+++ todo-w-reducer/src/components/ToDoList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ToDoItem from "./ToDoItem";
+
+const ToDoList = () => {
+  return (
+    <div>
+      <h2>TodoList</h2>
+      <ToDoItem />
+    </div>
+  )
+}
+export default ToDoList;
